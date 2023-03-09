4181180785ac36a0b4b08eb2e743f446d3df153d
Ivan <vankata294n4@gmail.com>
2023-03-09 20:04:52 +0200

    Implementing main structure:

--- youtube/src/App.js
+++ youtube/src/App.js
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {Box} from '@mui/material'
+import {Navbar, Feed, ChannelDetail, SearchFeed, VideoDetail} from './components'
 const App = () => {
   return (
-    <div>app</div>
+    <BrowserRouter>
+        <Box sx={{backgroundColor: '#000'}}>
+            <Navbar/>
+            <Routes>
+            <Route path='/' exact element={<Feed/>}/>
+            <Route path='/video/:id' element={<VideoDetail/>}/>
+            <Route path='/channel/:id' element={<ChannelDetail/>}/>
+            <Route path='/search/:search' element={<SearchFeed/>}/>
+            </Routes>
+        </Box>
+    </BrowserRouter>
   )
 }
 
