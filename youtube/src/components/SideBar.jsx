69abfa04298f97abe5927f6e982f5d6e1b6c6630
Ivan <vankata294n4@gmail.com>
2023-03-12 16:20:23 +0200

    Adding categories to sidebar

--- youtube/src/components/SideBar.jsx
+++ youtube/src/components/SideBar.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Stack } from '@mui/material'
+import { categories } from '../utils/contants'
 
-const SideBar = () => {
-  return (
-    <div>SideBar</div>
-  )
+export const SideBar = () => {
+  return <Stack direction="row" sx={
+    {overflowY: "auto", height: {sx: 'auto', md: '95%'}, flexDirection: {md: 'column'}}}>
+
+    {categories.map((category) => (
+        <button>
+            <span>{category.icon}</span>
+            <span>{category.name}</span>
+        </button>
+    ))}    
+    </Stack>
 }
 
 export default SideBar
