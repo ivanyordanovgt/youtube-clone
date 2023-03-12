ed232ccc3a3310f0c9efef867c0fbadbcb1664c3
Ivan <vankata294n4@gmail.com>
2023-03-12 15:35:40 +0200

    adding home Link with yt icon

--- youtube/src/components/Navbar.jsx
+++ youtube/src/components/Navbar.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import {Stack} from '@mui/material'
 import {Link} from 'react-router-dom'
+
+import { logo } from '../utils/contants'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+        {/* direction row because nav bar needs to be a row, p is padding */}
+        <Stack 
+        direction="row" 
+        alignItems="center" 
+        p={2} 
+        sx={{ position: 'sticky', background: '#000', top: 0, justifyContent: 'space-between'}}
+        >
+
+          <Link to="/" style={{'display': 'flex', alignItems: 'center'}}>
+            <img src={logo} style={{width: '3vw'}}></img>
+          </Link>
+        </Stack>
+    </div>
   )
 }
 
