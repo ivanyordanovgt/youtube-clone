132abb4636296adcde5db7d26fa31db1c411e4ea
Ivan <vankata294n4@gmail.com>
2023-03-12 16:12:08 +0200

    Adding sidebar component

--- youtube/src/components/Feed.jsx
+++ youtube/src/components/Feed.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import {Box, Stack, Typography} from "@mui/material"
+import SideBar from './SideBar'
 function Feed() {
   return (
     <Stack sx={{flexDirection: {sx: "column", md: "row"}}}>
@@ -8,7 +9,7 @@ function Feed() {
         height: {sx: 'auto', md: '92vh'}, 
         borderRight: '1px solid #3d3d3d', 
         px: {sx: 0, md: 2}}}>
-          SideBar
+          <SideBar></SideBar>
           <Typography className='copyright' variant='body2' sx={{mt: 1.5, color: '#fff'}}>
             Copyright 2023 
           </Typography>
