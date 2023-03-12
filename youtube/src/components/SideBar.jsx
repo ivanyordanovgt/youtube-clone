b0ded5c8934a95ebedc8a7832f65e0035399a658
Ivan <vankata294n4@gmail.com>
2023-03-12 16:35:16 +0200

    stying feedbar

--- youtube/src/components/SideBar.jsx
+++ youtube/src/components/SideBar.jsx
@@ -2,14 +2,19 @@ import React from 'react'
 import { Stack } from '@mui/material'
 import { categories } from '../utils/contants'
 
+const selectedCategory = 'New'; 
+
 export const SideBar = () => {
   return <Stack direction="row" sx={
     {overflowY: "auto", height: {sx: 'auto', md: '95%'}, flexDirection: {md: 'column'}}}>
 
     {categories.map((category) => (
-        <button>
-            <span>{category.icon}</span>
-            <span>{category.name}</span>
+        <button className="category-btn" style={{
+            background: category.name === selectedCategory && '#FC1503', color: 'rgba(255, 255, 255)'}} key={category.name}>
+            <span style={{
+                color: category.name === selectedCategory ? 'white': 'red', 
+                marginRight: '1vw'}}>{category.icon}</span>
+            <span style={{color: category.name === selectedCategory ? 'white': 'rgba(255,255,255,.8)'}}>{category.name}</span>
         </button>
     ))}    
     </Stack>
