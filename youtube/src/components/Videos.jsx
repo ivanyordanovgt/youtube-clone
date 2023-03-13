615233c559ae5c3a8a093a079572b22abbc11e3f
Ivan <vankata294n4@gmail.com>
2023-03-13 20:14:44 +0200

    Added components

--- youtube/src/components/Videos.jsx
+++ youtube/src/components/Videos.jsx
@@ -1,10 +1,17 @@
+import { Stack } from '@mui/system'
 import React from 'react'
 
 const Videos = ({videos}) => {
 
     console.log(videos)
   return (
-    <div>Videos</div>
+    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}> 
+        {videos.map((item, index) => {
+            <Box key={index}>
+                {item.id.videoId && <VideoCard video={item}/>}
+            </Box>
+        })}
+    </Stack>
   )
 }
 
