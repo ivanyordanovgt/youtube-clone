3dd2bbb182a05f007650821084ea3bc3cfaed279
Ivan <vankata294n4@gmail.com>
2023-03-13 18:47:20 +0200

    styling feed component

--- youtube/src/components/Feed.jsx
+++ youtube/src/components/Feed.jsx
@@ -14,6 +14,14 @@ function Feed() {
             Copyright 2023 
           </Typography>
         </Box>
+        <Box p={2} sx={{overflowY: 'auto', height: '90vh', flex: 2}}>
+          <Typography variant='h4' fontWeight='bold' mb={2} sx={{color: 'white'}}>
+            New <span style={{color: '#31503'}}>Videos</span>
+          </Typography>
+
+          
+        </Box>
+        
     </Stack>
     )
 }
