67c6d229b08ff24392bc4d07684c1f79b97cc943
Ivan <vankata294n4@gmail.com>
2023-03-14 17:18:54 +0200

    Fetching channel details in detail component

--- youtube/src/components/ChannelDetail.jsx
+++ youtube/src/components/ChannelDetail.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import {useState, useEffect} from 'react';
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
+import {Videos, ChannelCard} from './'
+import { fetchAPI } from '../utils/fetchAPI';
 
 function ChannelDetail() {
+  const [channelDetail, setChannelDetail] = useState(null)
+
+  const {id} = useParams();
+
+  useEffect(() => {
+    fetchAPI(`channels?part="snippet&id=${id}`)
+    .then((data) => {setChannelDetail(data?.items[0])})
+  }, [id])
   return (
     <div>ChannelDetail</div>
   )
--- youtube/src/components/VideoCard.jsx
+++ youtube/src/components/VideoCard.jsx
@@ -5,7 +5,6 @@ import { CheckCircle } from '@mui/icons-material'
 
 import {deboThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle} from '../utils/contants'
 const VideoCard = ({ video: {id: {videoId}, snippet} }) => {
-  console.log(videoId, snippet)
   return (
     <Card sx={{width: {md: '300px', xs: '100%'}, boxShadow: 'none', borderRadius: 'none'}}>
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
@@ -18,16 +17,20 @@ const VideoCard = ({ video: {id: {videoId}, snippet} }) => {
       <CardContent sx={{backgroundColor: '#1e1e1e', height: '106px'}} >
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}> 
       <Typography variant="subtitle1" fontWeight="bold" color="#FFF" >
-        {snippet?.title?.slice(0, 60) || demoVideoTitle?.slice(0, 40) }
+        {snippet.channelTitle || demoVideoTitle?.slice(0, 40) }
       </Typography>
       </Link>
 
       <Link to={snippet?.channelId ? `/channel/channelId` : demoChannelUrl}> 
       <Typography variant="subtitle2" fontWeight="bold" color="gray" sx={{width: '23vw'}}>
-        {snippet?.ChannelTitle?.slice(0, 60) || demoChannelTitle?.slice(0, 40) }
+        {snippet?.channelTitle?.slice(0, 60) || demoChannelTitle?.slice(0, 40) }
         <CheckCircle sx={{fontSize: 12, color: 'gray', ml: '5px'}}></CheckCircle>
       </Typography>
       </Link>
+
+      <Typography sx={{color: 'gray'}}>
+        {snippet?.description?.slice(0, 90)} {snippet?.description?.length > 90 ? '...': ' '}
+      </Typography>
       </CardContent>
     </Card>
   )
