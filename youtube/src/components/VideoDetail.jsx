9a47b37338f7cca80b5099a474061737fdfd8d59
Ivan <vankata294n4@gmail.com>
2023-03-20 16:02:07 +0200

    Destructuring videoDetail

--- youtube/src/components/VideoDetail.jsx
+++ youtube/src/components/VideoDetail.jsx
@@ -15,6 +15,8 @@ const VideoDetail = () => {
     .then((data) => setVideoDetail(data.items[0]))
   }, [id])
 
+  const videoData = videoDetail?.snippet
+
   return (
     <Box minHeight="95vh">
       <Stack direction={{xs: 'column', md: 'row'}}>
@@ -22,7 +24,7 @@ const VideoDetail = () => {
           <Box sx={{width: '100%', position: 'sticky', top: '86px'}}>
             <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className="react-player" controls/>
             <Typography color="#fff" variant='h5' fontWeight="bold" p={2}>
-              {videoDetail?.snippet?.title}
+              {videoData?.title}
             </Typography>
           </Box>
         </Box>
