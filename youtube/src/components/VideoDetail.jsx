1b0d6fbc369b0515ec005872c15f32ae42cea431
Ivan <vankata294n4@gmail.com>
2023-03-20 15:53:36 +0200

    Created video component structure

--- youtube/src/components/VideoDetail.jsx
+++ youtube/src/components/VideoDetail.jsx
@@ -4,11 +4,19 @@ import { Link, useParams } from 'react-router-dom'
 import ReactPlayer from 'react-player'
 import { Typography, Box, Stack } from '@mui/material'
 import { CheckCircle } from '@mui/icons-material'
-import Video from './'
+import Video from './Video'
 import { fetchAPI } from '../utils/fetchAPI'
 const VideoDetail = () => {
   return (
-    <div>VideoDetail</div>
+    <Box minHeight="95vh">
+      <Stack direction={{xs: 'column', md: 'row'}}>
+        <Box flex={1}>
+          <Box sx={{width: '100%', position: 'sticky', top: '86px'}}>
+            <ReactPlayer/>
+          </Box>
+        </Box>
+      </Stack>
+    </Box>
   )
 }
 
