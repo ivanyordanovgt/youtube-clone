1a325856f3e183797be00fd41058125dc3372e02
Ivan <vankata294n4@gmail.com>
2023-03-20 16:56:07 +0200

    Adding loader to videos component if no videos are passed

--- youtube/src/components/Videos.jsx
+++ youtube/src/components/Videos.jsx
@@ -4,6 +4,9 @@ import ChannelCard from './ChannelCard'
 import VideoCard from './VideoCard'
 
 const Videos = ({ videos, direction }) => {
+
+  if (!videos?.length) return "Loading..."
+
   return (
     <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
       {videos?.map((item, idx) => (
