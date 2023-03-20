6ad5e6c133330fc014aa23f269107db41bcaaa2e
Ivan <vankata294n4@gmail.com>
2023-03-20 16:34:29 +0200

    styling

--- youtube/src/components/VideoDetail.jsx
+++ youtube/src/components/VideoDetail.jsx
@@ -24,11 +24,12 @@ const VideoDetail = () => {
         <Box flex={1}>
           <Box sx={{width: '100%', position: 'sticky', top: '86px'}}>
             <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className="react-player" controls/>
-            <Typography color="#fff" variant='h5' fontWeight="bold" p={2}>
+            <Typography color="#fff" variant='h5' fontWeight="bold" p={2}> {title}
             </Typography>
             <Stack direction='row' justifyContent="space-between" sx={{color: '#fff'}} py={1} px={2}>
               <Link to={`/channel/${channelId}`}>
-                <Typography>{channelTitle}</Typography>
+                <Typography variant={{sm: 'subtitle1', md: 'h6'}} color='#fff'>{channelTitle}</Typography>
+                <CheckCircle sx={{fontSize: '12px', color: 'gray', ml: '5px'}}></CheckCircle>
               </Link>
             </Stack>
           </Box>
