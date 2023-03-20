23aee9c86cfbe087f3768febb42baf98b326623d
Ivan <vankata294n4@gmail.com>
2023-03-20 16:03:34 +0200

    Changing the method of destructuring the video data

--- youtube/src/components/VideoDetail.jsx
+++ youtube/src/components/VideoDetail.jsx
@@ -15,7 +15,7 @@ const VideoDetail = () => {
     .then((data) => setVideoDetail(data.items[0]))
   }, [id])
 
-  const videoData = videoDetail?.snippet
+  const { snippet } = videoDetail
 
   return (
     <Box minHeight="95vh">
@@ -24,7 +24,7 @@ const VideoDetail = () => {
           <Box sx={{width: '100%', position: 'sticky', top: '86px'}}>
             <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className="react-player" controls/>
             <Typography color="#fff" variant='h5' fontWeight="bold" p={2}>
-              {videoData?.title}
+              {snippet?.title}
             </Typography>
           </Box>
         </Box>
