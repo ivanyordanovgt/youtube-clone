3e130c248320869d9dea7b86a4cb18fbdf38b89e
Ivan <vankata294n4@gmail.com>
2023-03-20 16:06:53 +0200

    reverting method of getting the snippet from videoDetail due to an error caused by the fact snippet variable is getting set before useEffect can set the value to videoDetail

--- youtube/src/components/VideoDetail.jsx
+++ youtube/src/components/VideoDetail.jsx
@@ -15,7 +15,7 @@ const VideoDetail = () => {
     .then((data) => setVideoDetail(data.items[0]))
   }, [id])
 
-  const { snippet } = videoDetail
+  const videoData = videoDetail?.snippet
 
   return (
     <Box minHeight="95vh">
@@ -24,7 +24,7 @@ const VideoDetail = () => {
           <Box sx={{width: '100%', position: 'sticky', top: '86px'}}>
             <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className="react-player" controls/>
             <Typography color="#fff" variant='h5' fontWeight="bold" p={2}>
-              {snippet?.title}
+              {videoData?.title}
             </Typography>
           </Box>
         </Box>
