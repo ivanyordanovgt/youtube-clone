b8bb2f193aff5c1048ab4c15a96225c8c412047f
Ivan <vankata294n4@gmail.com>
2023-03-29 18:24:35 +0300

    Created an authapi

---
+++ youtube/src/apis/authAPI.js
@@ -0,0 +1,28 @@
+
+
+export class authAPI {
+  
+    url = 'http://127.0.0.1:8000/api/'
+  
+    post(apiUrl, data) {
+    }
+    
+    put(apiUrl, data) {
+    }
+  
+    get(apiUrl) {
+    }
+  
+    getUser() {
+    }
+  
+    isLoggedIn() {
+  
+    //   return this.http.get('http://127.0.0.1:8000/api/user', {withCredentials: true})
+  
+    };
+  
+    login(data) {
+    //   return this.http.post('http://127.0.0.1:8000/api/login', data)
+    }
+  };
