4f4ff99981f408058cdc252b5b5cf72c04ce564b
Ivan <vankata294n4@gmail.com>
2023-03-29 18:31:11 +0300

    added form template

---
+++ youtube/src/components/Login.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react'
+
+const Login = () => {
+
+    const [formValues, setFormValues] = useEffect({
+        'email': '',
+        'password': '',
+    })
+    const onSubmit = () => {
+
+    }
+    
+  return (
+    <div>
+           <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <TextField id="filled-basic" label="Filled" variant="filled" />
+      <TextField id="standard-basic" label="Standard" variant="standard" />
+    </Box>
+    </div>
+  )
+}
+
+export default Login
