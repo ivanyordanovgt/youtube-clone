3b4797253c1ea189741295289ba668442cc90d4e
Ivan <vankata294n4@gmail.com>
2023-03-29 18:58:56 +0300

    Register component sends correct reqs

--- youtube/src/apis/authAPI.js
+++ youtube/src/apis/authAPI.js
@@ -1,31 +1,32 @@
+import axios from "axios";
 
-
-export class authAPI {
+export const authAPI = {
   
-    url = 'http://127.0.0.1:8000/api/'
+    url: 'http://127.0.0.1:8000/api/',
   
     post(apiUrl, data) {
+        console.log(data)
         const result = axios({
             method: 'post',
             url: this.url + apiUrl,
             data: data
           });
-    }
+    },
     
     put(apiUrl, data) {
-    }
+    },
   
     get(apiUrl) {
-    }
+    },
   
     getUser() {
-    }
+    },
   
     isLoggedIn() {
   
     //   return this.http.get('http://127.0.0.1:8000/api/user', {withCredentials: true})
   
-    };
+    },
   
     login(data) {
     //   return this.http.post('http://127.0.0.1:8000/api/login', data)
--- youtube/src/components/Login.jsx
+++ youtube/src/components/Login.jsx
@@ -1,33 +1,43 @@
 import React, { useEffect, useState } from 'react'
 import { TextField, Box, Button } from '@mui/material'
+import { authAPI } from '../apis/authAPI'
+
 const Login = () => {
     console.log('register!  ')
     const [formValues, setFormValues] = useState({
         'email': '',
         'password': '',
     })
-    const onSubmit = () => {
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+        console.log("register")
+        if (formValues.email && formValues.password) {
+            authAPI.post('register', formValues)
+        } else alert('no no')
 
     }
 
     const onChangeHandler = (e) => {
-        setFormValues(state => ({...state, [e.target.name]: [e.target.value]}))
+        setFormValues(state => ({...state, [e.target.name]: e.target.value}));
         console.log(e.target.value)
     }
     
   return (
     <div style={{backgroundColor: 'white', marginTop: '20vh'}}>
-           <Box
+    
+    <Box
       component="form"
+      onSubmit={onSubmitHandler}
       sx={{
         '& > :not(style)': { m: 1, width: '25ch' },
       }}
       noValidate
       autoComplete="off"
     >
+    
       <TextField onChange={onChangeHandler} label="Email" variant="filled" name="email" value={formValues.email}/>
       <TextField onChange={onChangeHandler} label="Password" variant="filled" name="password" value={formValues.password}/>
-      <Button variant="outlined" style={{width: '7vw'}}>Register</Button>
+      <Button variant="outlined" type='submit' style={{width: '7vw'}}>Register</Button>
     </Box>
     </div>
   )
