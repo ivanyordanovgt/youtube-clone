07978ef707a954cd8299a4ef8801a04d90c10920
Ivan <vankata294n4@gmail.com>
2023-03-29 17:24:00 +0300

    Adding profile menu component

--- youtube/src/components/Navbar.jsx
+++ youtube/src/components/Navbar.jsx
@@ -3,23 +3,24 @@ import {Stack} from '@mui/material'
 import {Link} from 'react-router-dom'
 import { logo } from '../utils/contants'
 import { SearchBar } from './searchBar'
+import ProfileMenu from './ProfileMenu'
 const Navbar = () => {
   return (
     <div>
         {/* direction row because nav bar needs to be a row, p is padding */}
         <Stack 
-        direction="row" 
-        alignItems="center" 
-        p={2} 
-        sx={{ position: 'sticky', background: '#000', top: 0, justifyContent: 'space-between'}}
+       direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}
         >
 
-          <Link to="/" style={{'display': 'flex', alignItems: 'center'}}>
+          <Link to="/" style={{ display: "flex", alignItems: "center" }}>
             <img src={logo} style={{width: '3vw'}}></img>
           </Link>
+            <SearchBar sx={{position: 'absolute'}} ></SearchBar>
+            <ProfileMenu></ProfileMenu>
 
-          <SearchBar></SearchBar>
         </Stack>
+
+        
     </div>
   )
 }
