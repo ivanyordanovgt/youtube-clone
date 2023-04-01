aef7741bf67603982ba7b8fe274bdf34aff29c91
Ivan <vankata294n4@gmail.com>
2023-04-01 18:34:51 +0300

    Added buttons to profile

--- youtube/src/components/user/Profile.jsx
+++ youtube/src/components/user/Profile.jsx
@@ -1,6 +1,6 @@
 import {useState, useEffect} from 'react';
 import { useParams } from 'react-router-dom';
-import { Box } from '@mui/material';
+import { Box, Button } from '@mui/material';
 import {Videos, ChannelCard} from '..'
 import { fetchAPI } from '../../utils/fetchAPI';
 import { axiosClient } from '../../apis/axiosClient';
@@ -29,35 +29,6 @@ function ProfileDetail(pylaUser) {
               
             }))
     }) 
-    // fetchAPI(`channels?part="snippet&id=${id}`)
-    // .then((data) => {
-    //   const dataValues = data?.items[0];
-    //   setChannelDetails((state) => ({...state, 
-    //     'thubmnail': dataValues?.snippet?.thumbnails?.high?.url,
-    //     'title': dataValues?.snippet?.title,
-    //     subscribersCount: dataValues?.statistics?.subscriberCount,
-        
-    //   }))})
-    
-    // fetchAPI(`search?channelId=${id}&part=snippet&order=date`)
-    // .then((data) => {
-    //   console.log(data?.items)
-    //   const videos = [];
-    //   for (let video of data?.items) {
-    //     videos.push(
-    //       {
-    //         'id': video.id.videoId,
-    //         'channelId': video.snippet?.channelId,
-    //         'channelTitle': video.snippet?.channelTitle,
-    //         'description': video.snippet?.description,
-    //         'thubmnail': video?.snippet?.thumbnails.high.url,
-    //         'title': video.snippet?.title,
-    //       }
-    //     )
-    //   }
-
-    //   console.log(videos, '!!!VIDS')
-    //   setVideos(videos)})
   }, [id, channelDetails])
   return (
     <Box minHeight="95vh">
@@ -66,9 +37,14 @@ function ProfileDetail(pylaUser) {
       <Box>
         <div style={{
           background: 'linear-gradient(90deg, rgba(2,0,36,1) 7%, rgba(180,58,179,1) 49%, rgba(85,25,25,1) 100%)', 
-          zIndex: 10, height: '300px'}}>HAHAHA</div>
+          zIndex: 10, height: '250px'}}></div>
         <ChannelCard channelDetail={channelDetails} marginTop='-93px'></ChannelCard>
-      </Box>
+        <Box sx={{marginLeft: '45.5vw'}}>
+          <Button sx={{mb: '2vh', width: '9vw'}}variant='outlined'>Edit Profile</Button>
+          <br></br>
+          <Button variant='outlined' sx={{width: '9vw'}}>Post a video</Button>
+        </Box>
+        </Box>
 
       <Box display="flex" p="2">
         <Box sx={{mr: {sm: '100px'}}}/>
