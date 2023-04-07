945515db054f359453a3e6881a078ee510ce854a
Ivan <vankata294n4@gmail.com>
2023-04-07 17:02:36 +0300

    styles

--- youtube/src/components/videos/VideoDetail.jsx
+++ youtube/src/components/videos/VideoDetail.jsx
@@ -47,20 +47,20 @@ const VideoDetail = ({nonYoutube, video}) => {
   const {snippet: {title, channelId, channelTitle}, statistics: {viewCount, likeCount}} = videoDetail
 
   return (
-    <Box minHeight="95vh">
+    <Box minHeight="105vh">
       <Stack direction={{xs: 'column', md: 'row'}}>
         <Box flex={1}>
-          <Box sx={{width: '100%', position: 'sticky', top: '86px'}}>
+          <Box sx={{width: '100%', height: "50%", position: 'sticky', top: '86px'}}>
             <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className="react-player" controls/>
-            <Typography color="#fff" variant='h5' fontWeight="bold" p={2}> {title}
+            <Typography color="#fff" variant='h5' fontWeight="bold" sx={{paddingLeft: '2vw', paddingTop: '1vh'}}> {title}
             </Typography>
             <Stack direction='row' justifyContent="space-between" sx={{color: '#fff', float: 'left'}} py={1} px={2}>
               <Link to={`/channel/${channelId}`}>
-                <Typography variant={{sm: 'subtitle1', md: 'h6'}} color='#fff'>{channelTitle}</Typography>
+                <Typography variant={{sm: 'subtitle1', md: 'h7'}} color='#fff'>{channelTitle}</Typography>
                 <CheckCircle sx={{fontSize: '12px', color: 'gray', ml: '5px'}}></CheckCircle>
               </Link>
             </Stack>
-            <Stack direction="row" gap="20px" alignItems="center" sx={{float: 'right'}}>
+            <Stack direction="row" gap="20px" alignItems="center" sx={{float: 'right', height: '5vh'}}>
               <Typography variant='body1' sx={{opacity: 0.7, color: '#fff'}}>
                 {parseInt(viewCount).toLocaleString()} views
               </Typography>
